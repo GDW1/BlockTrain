eba751a81a12ef869d39d95f9b11c28c8ac6849c
Daryl Ou <darylou@169-231-171-108.wireless.ucsb.edu>
2021-10-15 23:23:15 -0700

    Formatted train cars in display

--- blocktrain/src/components/Display.jsx
+++ blocktrain/src/components/Display.jsx
@@ -1,18 +1,56 @@
 import React, { useState } from 'react'
 import './Display.css';
 
+const sampleData = ["he", "can", "go", "and", "she", "can", "to"]
+
 function Display() {
 
     return (
-        <div className="helloDiv">
-            <h1> hi </h1>
-            <p> hello</p>
-            <div id="subHelloDiv">
-                <h1> hi </h1>
-                <p> hello</p>
+        <div className={"displayComponent"}>
+            <h1>hi</h1>
+            <div className="train">
+                <h1> Train </h1>
+                <table id = "trainCars">
+                    <tr>
+                        <td>
+                            <div>
+                                <p>{sampleData[0]}</p>
+                            </div>
+                        </td>
+                        <td>
+                            <div>
+                                <p>can</p>
+                            </div>
+                        </td>
+                        <td>
+                            <div>
+                                <p>go</p>
+                            </div>
+                        </td>
+                        <td>
+                            <div>
+                                <p>and</p>
+                            </div>
+                        </td>
+                        <td>
+                            <div>
+                                <p>so</p>
+                            </div>
+                        </td>
+                        <td>
+                            <div>
+                                <p>can</p>
+                            </div>
+                        </td>
+                        <td>
+                            <div>
+                                <p>she</p>
+                            </div>
+                        </td>
+                    </tr>
+                </table>
             </div>
         </div>
-
     )
 }
 
