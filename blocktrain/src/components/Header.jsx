c1013fa220802a0a0927edebc4a945db8c0a3ccd
Daryl Ou <darylou@ucsb.edu>
2021-10-21 21:42:40 -0700

    darylou - created header jsx and css, changed the colors, added header to app.js

---
+++ blocktrain/src/components/Header.jsx
@@ -0,0 +1,17 @@
+/* Filename: Header.jsx; Last Updated: 10/21/2021
+ * Header.jsx displays the logo and top bar
+ */
+
+import React, { useState } from 'react'
+import './Header.css';
+
+function Header() {
+
+    return (
+        <div className = "headerBlock">
+            <p><span className = "bold">Block</span>Train</p>
+        </div>
+    )
+}
+
+export default Header
