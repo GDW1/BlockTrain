99d64bb581b0a4acf0f224db8ed0f510e9c68718
Ryan He <ryanhxhe@gmail.com>
2021-10-24 10:58:17 -0700

    take data from blockchain and store in a json

--- blockrest/index.js
+++ blockrest/index.js
@@ -5,23 +5,75 @@ const app = express();
 const port = 9000;
 const readline = require('readline-sync');
 const axios = require('axios'); 
-// const router = express.Router();
+const fs = require("fs"); 
+const onewordstory = require('./onestorywords');
+// const FormData = require('form-data');
 
 let blockchainCount = 1;
 const blockchain = new Blockchain();
-blockchain.chain[0].data = ""; 
+blockchain.chain[0].data = "";
+
+var viewData = { 
+    blockchaindata : [] 
+}; 
 
 app.use(express.urlencoded({extended: true})); 
+app.use(express.json());
 
 app.post('/', (req, res) => { 
-    let word = req.body.userInput;
-    const newData = word;
+   //  const form = new FormData(); 
+    const newData = req.body.userInput;
     blockchain.addBlock({data: newData});
-    console.log(blockchain.chain[blockchainCount].data);           
-    let loopThroughBlockChain = 1;  
+    let currBlockData = blockchain.chain[blockchainCount].data;
+    let newWord = {
+        "word" : currBlockData
+    }
+    onewordstory.push(newWord); 
+    fs.writeFile("onestorywords.json", JSON.stringify(onewordstory), err => {
+     
+        // Checking for errors
+        if (err) throw err; 
+       
+        console.log("Done writing"); // Success
+    });
+    // const file = fs.readFile("onestorywords.json", err => {
+     
+    //     // Checking for errors
+    //     if (err) throw err; 
+       
+    //     console.log("Done reading"); // Success);
+    // });
+    // form.append('file', file, "onestorywords.json");
+    // console.log(form.getHeaders());
+
+    console.log(currBlockData);             
     blockchainCount++;
     console.log();
 })
+
+// app.get('/', function(req, res, next) {
+//     let objArray = [];
+//     const file = fs.readFile("onestorywords.json", err => {
+     
+//         // Checking for errors
+//         if (err) throw err; 
+        
+//         console.log("Done reading"); // Success);
+//     });
+//     objArray.push(file);
+//     res.render('index', { data: JSON.stringify(objArray) })
+// });
+
+
+app.get('/onewordstoryjson', function (req, res) {
+    res.header("Content-Type",'application/json');
+    res.send(JSON.stringify(onewordstory));
+})
+
+// app.get('/search', (req, res) => {
+//     res.header("Content-Type",'application/json');
+//     res.sendFile(path.join(__dirname, './onestorywords.json'));
+// })
 
 app.listen(port, () => {
 	console.log('Server is running...');
--- blocktrain/src/components/Display.jsx
+++ blocktrain/src/components/Display.jsx
@@ -5,65 +5,132 @@
 import React, { useState } from 'react'
 import './Display.css';
 import trainArt from '../images/TrainCar.png'
+import axios from 'axios';
 
 // SAMPLE DATA. CHANGE WHEN REAL DATA AVAILABLE
 const sampleData = ["he", "can", "go", "and", "she", "can", "too"]
 
-// NOTE: NEED TO ENSURE THAT CHARACTERS ARE ONLY 10 CHARS LONG AND TRAIN CAR CAN ACCOMMODATE THAT
-function Display() {
+// div(data-objData=data);
 
-    return (
-        <div className={"displayComponent"}>
-            <div className="train">
-                <table id = "trainCars">
-                    <tr>
-                        <td>
-                            <div>
-                                <img className = "trainCarImage" src = {trainArt} alt = "train car" />
-                                <p>{sampleData[0]}</p>
-                            </div>
-                        </td>
-                        <td>
-                            <div>
-                                <img className = "trainCarImage" src = {trainArt} alt = "train car" />
-                                <p>{sampleData[1]}</p>
-                            </div>
-                        </td>
-                        <td>
-                            <div>
-                                <img className = "trainCarImage" src = {trainArt} alt = "train car" />
-                                <p>{sampleData[2]}</p>
-                            </div>
-                        </td>
-                        <td>
-                            <div>
-                                <img className = "trainCarImage" src = {trainArt} alt = "train car" />
-                                <p>{sampleData[3]}</p>
-                            </div>
-                        </td>
-                        <td>
-                            <div>
-                                <img className = "trainCarImage" src = {trainArt} alt = "train car" />
-                                <p>{sampleData[4]}</p>
-                            </div>
-                        </td>
-                        <td>
-                            <div>
-                                <img className = "trainCarImage" src = {trainArt} alt = "train car" />
-                                <p>{sampleData[5]}</p>
-                            </div>
-                        </td>
-                        <td>
-                            <div>
-                                <img className = "trainCarImage" src = {trainArt} alt = "train car" />
-                                <p>{sampleData[6]}</p>
-                            </div>
-                        </td>
-                    </tr>
-                </table>
+const api = axios.create({
+    baseURL: `http://localhost:9000/onewordstoryjson`
+})
+
+// NOTE: NEED TO ENSURE THAT CHARACTERS ARE ONLY 10 CHARS LONG AND TRAIN CAR CAN ACCOMMODATE THAT
+
+// SAMPLE DATA. CHANGE WHEN REAL DATA AVAILABLE
+
+class Display extends React.Component{
+    // constructor(props) {
+    //     super(props);
+    //     api.get('/').then(res => {
+    //         console.log(res.data);
+    //         console.log('Hello World');
+    //         this.state = {sampleData: ["", "", "", "", "", "", ""]};
+    //     })
+    // }
+
+    constructor(props){
+        super(props);
+        //console.log(this.props);
+        this.state = {sampleData: ["", "", "", "", "", "", ""]};
+        api.get('/').then(res => {
+            console.log(res.data);
+            console.log('Hello World');
+        })
+    };
+    pullData(){
+        // //console.log("pulling");
+        // const url = "http://localhost:3000/trainwords";
+        // axios.get(url)
+        //     .then(
+        //         (res) => {
+        //             let rawData = JSON.parse(JSON.stringify(res.data));
+        //             let newData = [];
+        //             for (let i = 0; i < 7; i++){
+        //                 newData.push(rawData[rawData.length - 1 - i].word);
+        //             }
+
+        //             // this.sampleData = newData;
+        //             // this.forceUpdate();
+        //             this.setState({sampleData: newData});
+                    
+        //         }
+
+        //     )
+        console.log("Help");
+    }
+    componentDidMount(){
+        this.checkInterval = setInterval(() => {
+            //SET BACKEND URL HERE
+            this.pullData();
+        }, 3000);
+    }
+    componentWillUnmount(){
+        clearInterval(this.checkInterval);
+    }
+    render() {
+        //console.log(this.props.update);
+        //console.log("rendering");
+        if (this.props.update){
+            //console.log("updating");
+            this.pullData();
+            this.props.setUpdate(false);
+        }
+        return (
+            <div className={"displayComponent"}>
+                <div className="train">
+                    <table id = "trainCars">
+                        <tr>
+                            <td>
+                                <div>
+                                    <img className = "trainCarImage" src = {trainArt} alt = "train car" />
+                                    <p>{this.state.sampleData[6]}</p>
+                                </div>
+                            </td>
+                            <td>
+                                <div>
+                                    <img className = "trainCarImage" src = {trainArt} alt = "train car" />
+                                    <p>{this.state.sampleData[5]}</p>
+                                </div>
+                            </td>
+                            <td>
+                                <div>
+                                    <img className = "trainCarImage" src = {trainArt} alt = "train car" />
+                                    <p>{this.state.sampleData[4]}</p>
+                                </div>
+                            </td>
+                            <td>
+                                <div>
+                                    <img className = "trainCarImage" src = {trainArt} alt = "train car" />
+                                    <p>{this.state.sampleData[3]}</p>
+                                </div>
+                            </td>
+                            <td>
+                                <div>
+                                    <img className = "trainCarImage" src = {trainArt} alt = "train car" />
+                                    <p>{this.state.sampleData[2]}</p>
+                                </div>
+                            </td>
+                            <td>
+                                <div>
+                                    <img className = "trainCarImage" src = {trainArt} alt = "train car" />
+                                    <p>{this.state.sampleData[1]}</p>
+                                </div>
+                            </td>
+                            <td>
+                                <div>
+                                    <img className = "trainCarImage" src = {trainArt} alt = "train car" />
+                                    <p>{this.state.sampleData[0]}</p>
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
+// NOTE: NEED TO ENSURE THAT CHARACTERS ARE ONLY 10 CHARS LONG AND TRAIN CAR CAN ACCOMMODATE THAT
 
 export default Display
