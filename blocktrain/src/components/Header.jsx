f0873d1329e8d079cf145e69742c9a199480b2c4
Daryl Ou <darylou@ucsb.edu>
2021-10-23 18:01:03 -0700

    added barebone button and dropdown mennu

--- blocktrain/src/components/Header.jsx
+++ blocktrain/src/components/Header.jsx
@@ -9,6 +9,14 @@ function Header() {
 
     return (
         <div className = "headerBlock">
+            <div className="dropdown">
+                <button className="dropbtn">Dropdown</button>
+                <div className="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                </div>
+            </div>
             <p><span className = "bold">Block</span>Train</p>
         </div>
     )
