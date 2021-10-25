ebd0bceb0628e17eac3099a932c3cf38d1f0be06
Daryl Ou <darylou@ucsb.edu>
2021-10-24 17:05:40 -0700

    added framework for about us page

--- blocktrain/src/pages/AboutUs.js
+++ blocktrain/src/pages/AboutUs.js
@@ -3,12 +3,47 @@
  */
 import './AboutUs.css';
 import Header from '../components/Header.jsx'
+import Profile from '../components/Profile.js'
 
 function AboutUs() {
     return (
         <div className="AboutUs">
             <Header/>
-            <h1>THIS IS THE ABOUT US PAGE</h1>
+            <h1>About Us</h1>
+            <table id = "profiles">
+                <tr>
+                    <td>
+                        <Profile profilePic = ""
+                                 name = "Daryl Ou"
+                                 description = ""/>
+                    </td>
+                    <td>
+                        <Profile profilePic = ""
+                                 name = "David Wang"
+                                 description = ""/>
+                    </td>
+                    <td>
+                        <Profile profilePic = ""
+                                 name = "Guy Wilks"
+                                 description = ""/>
+                    </td>
+                    <td>
+                        <Profile profilePic = ""
+                                 name = "Kyle Wong"
+                                 description = ""/>
+                    </td>
+                    <td>
+                        <Profile profilePic = ""
+                                 name = "Ryan He"
+                                 description = ""/>
+                    </td>
+                    <td>
+                        <Profile profilePic = ""
+                                 name = "Rohan Marangoly"
+                                 description = ""/>
+                    </td>
+                </tr>
+            </table>
         </div>
     );
 }
