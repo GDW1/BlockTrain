e3267477ffd1dee1126ba47a41faeaf3b0375e2e
Kyle Wong <knw@ucsb.edu>
2021-11-17 22:02:05 -0800

    updated genesis hash

--- blockrest/genesisBlock.js
+++ blockrest/genesisBlock.js
@@ -1,10 +1,15 @@
+const SHA256 = require('crypto-js/sha256');
+
 class GenesisBlock {
     constructor(timestamp, data = '') {
         this.timestamp = timestamp;
         this.data = data;
-        this.hash = '7b15ee6437cb4d6dbe2f5734af72f5e767a8c296b0f43999a6a587b2b93744d8'; // "Blocktrain" in sha256
+        this.hash = this.calculateHash();
         this.lastHash = null;
         this.lastBlock = null;
+    }
+    calculateHash() {
+        return SHA256(this.timestamp + JSON.stringify(this.data)).toString();
     }
 }
 module.exports = GenesisBlock;
